58e09ce8c63ecadb209ec29500ba276f7719183d
Amodoi Peter <amodoipeter@gmail.com>
2022-06-05 06:19:42 +0300

    print args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     else:
         print("{:d} arguments:".format(arguments))
         for i in range(1, len(argv)):
-        print("{:d}: {}".format(i, argv[i]))
+            print("{:d}: {}".format(i, argv[i]))
