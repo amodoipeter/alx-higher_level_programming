6fe5d83b2b6ede0c5e5134eaeb0fa5c8b9c0afde
Amodoi Peter <amodoipeter@gmail.com>
2022-06-05 06:15:11 +0300

    add ints

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     """Adds two  numbers"""
     from add_0 import add
-
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
