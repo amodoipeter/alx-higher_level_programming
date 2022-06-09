662573cecab41c54232b9b34a51470f6378106f5
amodoipeter <amodoipeter@gmail.com>
2022-06-09 03:24:49 +0300

    add result of args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
-
 if __name__ == "__main__":
-    import sys
-    add_arg(sys.argv)
+    from sys import argv
+    args = len(argv) - 1
+    sum = 0
+    if args is 0:
+        print("{:d}".format(0))
+    else:
+        for i in range(1, len(argv)):
+            sum += int(argv[i])
+        print("{:d}".format(sum))
