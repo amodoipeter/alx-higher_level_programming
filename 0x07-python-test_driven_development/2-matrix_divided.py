b5e3aafd8a40dc6790f2cc7ae09783e77ade1447
Amodoi Peter <amodoipeter@gmail.com>
2022-06-29 06:24:21 +0300

    division of matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 
 """
-a function that divides all elements of a matrix
+This module contains the matrix_divided function
 """
 
 
 def matrix_divided(matrix, div):
     """
-    DivideS all elements of a matrix. Return new matrix. The result is rounded to two decimal places
-    :param matrix:
-    :param div:
-    :return:
+    Divides all elements of a matrix. Returns a
+    new matrix (list of list). The result is
+    rounded to 2 decimal places.
     """
     if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
-        raise TypeError("matrix must be a matrix (list of lists)" + "of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) " +
+                        "of integers/floats")
 
     for row in matrix:
         if len(row) is 0:
-            raise TypeError("matrix must be a matrix (list of lists)" + "of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                            "of integers/floats")
 
     for i in row:
         if type(i) != int and type(i) != float:
-            raise TypeError("matrix must be a matrix (list of lists)" + "of integers//floats")
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                            "of integers/floats")
 
     len_rows = []
     for row in matrix:
         len_rows.append(len(row))
-    if not all(elements == len_rows[0] for elements in len_rows ):
-        raise TypeError("each row of the matrix must have the same size ")
+    if not all(elements == len_rows[0] for elements in len_rows):
+        raise TypeError("Each row of the matrix must have the same size")
 
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
@@ -35,6 +37,6 @@ def matrix_divided(matrix, div):
     if div is 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = [[round(i / div, 2) for i in row] for row in matrix]
+    NEW_matrix = [[round(i / div, 2) for i in row] for row in matrix]
 
-    return new_matrix
+    return NEW_matrix
