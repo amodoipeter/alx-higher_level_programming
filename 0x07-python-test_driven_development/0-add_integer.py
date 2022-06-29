be5ea83adb85cc4486ec034b6abfdeb4534312a9
Amodoi Peter <amodoipeter@gmail.com>
2022-06-29 03:55:04 +0300

    add ints

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+function that adds two integers
+"""
+
+def add_integer(a,b=98):
+    """
+    return sum of a and b. print an error message if a and b aren't integers or floats
+    :param a:
+    :param b:
+    :return:
+    """
+    if type(a)==float:
+        a = int(a)
+    elif type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b)==float:
+        b = int(b)
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
