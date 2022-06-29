d94c7e787749e7d03e31863ae9918d8e52de3ed3
Amodoi Peter <amodoipeter@gmail.com>
2022-06-29 06:06:52 +0300

    add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,18 +4,19 @@
 function that adds two integers
 """
 
-def add_integer(a,b=98):
+
+def add_integer(a, b=98):
     """
     return sum of a and b. print an error message if a and b aren't integers or floats
     :param a:
     :param b:
     :return:
     """
-    if type(a)==float:
+    if type(a) == float:
         a = int(a)
     elif type(a) != int:
         raise TypeError("a must be an integer")
-    if type(b)==float:
+    if type(b) == float:
         b = int(b)
     elif type(b) != int:
         raise TypeError("b must be an integer")
