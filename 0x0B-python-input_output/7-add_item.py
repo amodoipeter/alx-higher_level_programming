bee0cf478790e74a73d84ef450d6e32bd9841030
Amodoi Peter <amodoipeter@gmail.com>
2022-07-06 07:15:30 +0300

    load add and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list"""
-
-
+"""script that adds all arguments to a Python list
+"""
 import sys
+
 if __name__ == "__main__":
-    """add to json file"""
     save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('8-load_from_json_file').load_from_json_file
@@ -15,4 +14,3 @@ if __name__ == "__main__":
         items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
-
